a9302aeeaedabd94836ee03af422d2da63b3d413
Himanshu Jha <90416078+HIMANSHU-1504@users.noreply.github.com>
2024-09-07 22:37:32 +0530

    Firebase Authentication Added

--- app/src/main/java/com/example/brightfuture/auth/LoginActivity.java
+++ app/src/main/java/com/example/brightfuture/auth/LoginActivity.java
@@ -2,29 +2,83 @@ package com.example.brightfuture.auth;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.brightfuture.MainActivity;
 import com.example.brightfuture.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText loginEmail, loginPassword;
+    private TextView signupRedirectText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button registrationButton = findViewById(R.id.registerButton);
-        registrationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
 
-                //open registration page when Register Button CLicked
-                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
-                startActivity(intent);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String email = loginEmail.getText().toString();
+                String pass = loginPassword.getText().toString();
+
+                if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    if(!pass.isEmpty()){
+                        auth.signInWithEmailAndPassword(email, pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(LoginActivity.this, getString(R.string.loginSuccessful), Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, getString(R.string.loginFailed), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                    else{
+                        loginPassword.setError(getString(R.string.emptyPass));
+                    }
+                }
+                else if(email.isEmpty()){
+                    loginEmail.setError(getString(R.string.emptyEmail));
+                }
+                else{
+                    loginEmail.setError(getString(R.string.validEmail));
+                }
             }
         });
+
+            signupRedirectText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+                }
+            });
     }
 }
--- app/src/main/java/com/example/brightfuture/auth/RegistrationActivity.java
+++ app/src/main/java/com/example/brightfuture/auth/RegistrationActivity.java
@@ -1,15 +1,82 @@
 package com.example.brightfuture.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.brightfuture.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText signupEmail, signupPassword, signupConfirmPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        auth = FirebaseAuth.getInstance();
+        signupEmail = findViewById(R.id.signup_email);
+        signupPassword = findViewById(R.id.signup_password);
+        signupConfirmPassword = findViewById(R.id.signup_confirm_password);
+        Button signupButton = findViewById(R.id.signup_button);
+        TextView loginRedirectText = findViewById(R.id.loginRedirectText);
+
+        signupButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String user = signupEmail.getText().toString().trim();
+                String pass = signupPassword.getText().toString().trim();
+                String cnfPass = signupConfirmPassword.getText().toString().trim();
+
+                if(user.isEmpty()){
+                    signupEmail.setError(getString(R.string.emptyEmail));
+                }
+
+                if(pass.isEmpty()){
+                    signupPassword.setError(getString(R.string.emptyPass));
+                }
+
+                if(!pass.equals(cnfPass)){
+                    signupConfirmPassword.setError(getString(R.string.passwordsDoNotMatch));
+                }
+
+                else{
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegistrationActivity.this, getString(R.string.signUpSuccessful), Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+                            }
+                            else{
+                                Toast.makeText(RegistrationActivity.this, getString(R.string.signUpFailed) + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
