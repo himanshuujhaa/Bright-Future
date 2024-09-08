d14c95263322211b6583d601c2e33583b9f756a4
Himanshu Jha <90416078+HIMANSHU-1504@users.noreply.github.com>
2024-09-09 00:42:12 +0530

    Completed 10th grade work

--- app/src/main/java/com/example/brightfuture/MainActivity.java
+++ app/src/main/java/com/example/brightfuture/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.brightfuture;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.brightfuture.X.XActivity;
+import com.example.brightfuture.XII.XIIActivity;
 import com.example.brightfuture.auth.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this, "Successfull", Toast.LENGTH_SHORT).show();
+
+        Button xButton = findViewById(R.id.X_button);
+        Button xiiButton = findViewById(R.id.XII_button);
+
+        xButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, XActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        xiiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, XIIActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
